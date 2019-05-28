148bb894077aab80bee42f469a4577a8c1b6a8dc
macha <machvik1@gmail.com>
2019-05-28 02:39:46 -0400

    initial commit

---
+++ users/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateFrom
+from django.contrib.auth.decorators import login_required
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Successfully created an account. Log in now!')
+            return redirect('login')
+
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form, 'title':'register'})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateFrom(request.POST,
+                                     request.FILES,
+                                        instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Successfully updated profile!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateFrom(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
